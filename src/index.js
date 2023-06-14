1dd58c424dde8dc796211b12b442970540e205f5
Ritvik <mritvikprasad@gmail.com>
2023-06-14 13:20:25 +0530

    hash router instead of browser router

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React, { StrictMode } from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, HashRouter } from "react-router-dom";
 
 
 import "./index.css";
@@ -8,8 +8,8 @@ import AppRouter from "./AppRouter";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <AppRouter />
-    </BrowserRouter>
+    </HashRouter>
   </StrictMode>
 );
